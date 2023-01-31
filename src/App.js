b2c72523c9ea8a6b064fac1b59562184fe91491d
Fabien Delpierre <fab.delpierre@gmail.com>
2023-01-31 16:45:07 +0100

    createLogo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Recipe from './pages/Recipe';
 
+
 function App() {
     return (
+        
         <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />}/>
---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+
+const Logo = () => {
+    return (
+        <div className='logo'>          
+            <h2>Cookingapp</h2>
+        </div>
+    );
+};
+
+export default Logo;
